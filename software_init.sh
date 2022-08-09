b52d84327e5f266648c914952006bb70148a7c33
imagicw <wyw310@gmail.com>
2022-08-09 19:34:31 +0800

    调整安装顺序

--- software_init.sh
+++ software_init.sh
@@ -5,20 +5,28 @@ echo "正在安装 Homebrew..."
 
 echo "Homebrew 安装完成！	\n开始安装软件包..."
 
-brew install mimestream --cask
-brew install telegram --cask
-brew install mas
-brew install iina --cask
-brew install visual-studio-code --cask
+# 工具类
 brew install iterm2 --cask
-# brew install loopback
-# brew install audio-hijack
+brew install iina --cask
+brew install handbrake --cask
+# 效率类
+brew install mimestream --cask
 brew install notion --cask
+brew install visual-studio-code --cask
+
+# IM
+brew install telegram --cask
 brew install wechat --cask
 brew install discord --cask
+
+# brew install loopback
+# brew install audio-hijack
 # brew install jq
 
-echo "正在安装 mas 软件..."
+echo "正在安装 mas cli..."
+brew install mas
+
+echo "mas 安装完成！ \n开始安装 mas 软件..."
 mas install 1176895641 # Spark
 
 echo "软件安装完成！"
