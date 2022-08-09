95ab43344c9d5e8f9872138d173adf297253abe6
imagicw <wyw310@gmail.com>
2022-08-09 20:13:14 +0800

    加入执行安装确认

--- software_init.sh
+++ software_init.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# ctrl + c 无法中断整个程序的运行
+
+read -r -n1 -p "即将进行软件包的安装，是否继续？[y/n]: " confirm
+
+case $confirm in
+  [yY])
+    echo "\n开始安装软件包"
+    ;;
+
+  *)
+    echo "\n退出软件包安装"
+    exit
+    ;;
+esac;
+
 echo "正在安装 Homebrew..."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -9,6 +24,7 @@ echo "Homebrew 安装完成！	\n开始安装软件包..."
 brew install iterm2 --cask
 brew install iina --cask
 brew install handbrake --cask
+
 # 效率类
 brew install mimestream --cask
 brew install notion --cask
@@ -23,6 +39,8 @@ brew install discord --cask
 # brew install audio-hijack
 # brew install jq
 
+read -s -n1 -p "即将安装 Mac app store 中的应用，请确保 Apple ID 已经登录。按任意键继续..."
+
 echo "正在安装 mas cli..."
 brew install mas
 
@@ -30,3 +48,5 @@ echo "mas 安装完成！ \n开始安装 mas 软件..."
 mas install 1176895641 # Spark
 
 echo "软件安装完成！"
+
+exit 0
